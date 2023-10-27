862896e6a7df2e5eb2b8378160907de7618e85b5
J Quintero <99591800+JQuintero2430@users.noreply.github.com>
2023-10-27 07:50:13 -0500

    avance

--- src/main/java/com/jorge/springboot/web/app1/controllers/IndexController.java
+++ src/main/java/com/jorge/springboot/web/app1/controllers/IndexController.java
@@ -4,7 +4,11 @@ import com.jorge.springboot.web.app1.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping({"/app"})
@@ -18,12 +22,27 @@ public class IndexController {
 
     @GetMapping({"/profile1"})
     public String profile(Model model){
-        User user = new User("Jorge", "Gonzalez");
+        User user = new User("Jorge", "Gonzalez", "jorge@correo.com");
         model.addAttribute("user", user);
         model.addAttribute("title", "Perfil del usuario ".concat(user.getName()));
         model.addAttribute("h1", "Perfil del usuario ".concat(user.getName()
                                                                    .concat(" ")
                                                                    .concat(user.getSurname())));
         return "profile";
+    }
+
+    @GetMapping({"/listar"})
+    public String listar(Model model){
+        model.addAttribute("title", "Listado de usuarios");
+        return "listar";
+    }
+
+    @ModelAttribute("usuarios")
+    public List<User> fillUsers() {
+        return Arrays.asList(
+                new User("Jorge", "Gonzalez", "jorge@correo.com"),
+                new User("John", "Doe", "john@correo.com"),
+                new User("jane", "Lynch", "jane@correo.com"),
+                new User("Ray", "Charles", "ray@correo.com"));
     }
 }
